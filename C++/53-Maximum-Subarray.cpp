2738cf4b64bc6504c82aac419b0972efc66d67c9
Tanya Gupta <tanyagupta.pg@gmail.com>
2022-01-20 14:29:15 +0530

    Added Happy Number and Maximum subarray sum

---
+++ C++/53-Maximum-Subarray.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    int maxSubArray(vector<int> &nums)
+    {
+        int currSum = 0, maxSum = INT_MIN;
+        // Kadane's Algorithm for Maximum Subarray Sum
+        for (int i = 0; i < nums.size(); i++)
+        {
+            currSum += nums[i];
+            maxSum = max(maxSum, currSum);
+
+            if (currSum < 0)
+                currSum = 0;
+        }
+        return maxSum;
+    }
+};
