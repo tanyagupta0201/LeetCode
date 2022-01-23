72c2bf87d47f3c74c0926ddb211a7163f028bd38
Tanya Gupta <tanyagupta.pg@gmail.com>
2022-01-23 11:39:41 +0530

    Added koko eating banana in C++

---
+++ C++/875-Koko-Eating-Bananas.cpp
@@ -0,0 +1,56 @@
+// Problem Link: https://github.com/tanyagupta0201/6Companies30Days
+
+// YouTube Link: https://youtu.be/LzZFUTWE55c
+
+class Solution
+{
+public:
+    bool isPossible(vector<int> piles, int h, int sp)
+    {
+        int time = 0;
+
+        for (int i : piles)
+        {
+            // We need ceil value, suppose we get 2.3 so koko need to spend 3 hrs there but integer value will give 2 hrs as answer
+            time += (int)ceil(1.0 * i / sp);
+        }
+
+        if (time <= h)
+            return true;
+        return false;
+    }
+
+    int minEatingSpeed(vector<int> &piles, int h)
+    {
+
+        int maxi = INT_MIN;
+
+        for (int i : piles)
+        {
+            maxi = max(i, maxi);
+        }
+
+        // Agar number of hours  number of piles ke equal diye hue hain toh maximum value hi leni padegi usse choti value le hi nahi sakte
+        if (h == piles.size())
+            return maxi;
+
+        int low = 1, high = maxi;
+        int speed = INT_MIN;
+
+        while (low <= high)
+        {
+            int sp = low + (high - low) / 2; // to avoid overflow
+
+            if (isPossible(piles, h, sp) == true)
+            {
+                speed = sp;
+                high = sp - 1;
+            }
+            else
+            {
+                low = sp + 1;
+            }
+        }
+        return speed;
+    }
+};
