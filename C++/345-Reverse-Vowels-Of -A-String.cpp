9ae87c86a890d3b5f758a50921ccc6ee29c21074
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-01-23 18:30:22 +0530

    Added Reverse vowels of a string in C++

---
+++ C++/345-Reverse-Vowels-Of -A-String.cpp
@@ -0,0 +1,39 @@
+// Problem Link: https://leetcode.com/problems/reverse-vowels-of-a-string/
+
+class Solution {
+public:
+    bool isVowel(char ch)
+    {
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+            return true;
+        
+        if(ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            return true;
+        
+        return false;
+    }
+    
+    string reverseVowels(string s) 
+    {    
+        int low = 0, high = s.length() - 1;
+        
+        while(low < high)
+        {
+            if(!isVowel(s[low]))
+            {
+               low++;
+            }
+            else if(!isVowel(s[high]))
+            {
+                high--;
+            }
+            else 
+            {
+                swap(s[high], s[low]);
+                low++;
+                high--;
+            }
+        }
+        return s;
+    }
+};
