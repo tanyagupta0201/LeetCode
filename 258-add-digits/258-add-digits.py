7aa9563ef2b8fa47bd2503e2910435f0a5de749b
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-02-01 18:38:15 +0530

    Update 258-add-digits.py

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -8,3 +8,13 @@ class Solution:
                 num = num + int(i)
                 
         return num
+    
+    
+# The original number is divisible by 9 if and only if the sum of its digits is divisible by 9
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
