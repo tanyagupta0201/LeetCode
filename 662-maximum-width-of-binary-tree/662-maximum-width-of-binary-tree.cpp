ce29966820d761f04042b61defa24f8e147f2174
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-02-27 13:54:09 +0530

    Time: 0 ms (100.00%), Space: 17.5 MB (21.15%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -11,44 +11,36 @@
  */
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) 
-    {
-        if(root == NULL)
+    int widthOfBinaryTree(TreeNode* root) {
+        if(!root)
             return 0;
-        
-        int ans = 0;
-        
-        queue<pair<TreeNode*, int>> q;
-        q.push({root, 0});
+        long long ans = 0;
+        queue<pair<TreeNode*, long long>> q;
+        q.push({root,0});
         
         while(!q.empty())
         {
             int size = q.size();
-            int currMin = q.front().second;
+            //to make the id starting from zero
+            long long mmin = q.front().second;    
+            long long first,last;
             
-            int leftMost, rightMost;
-            
-            for(int i = 0; i < size; i++)
-            {
-                // To prevent integer overflow, subtraction is done
-                int cur_id = q.front().second - currMin;
-                
-                TreeNode* temp = q.front().first;
+            for(int i=0; i<size; i++){
+                // avoiding overflow by subtrcting min value of a level
+                long cur_id = q.front().second - mmin;
+                TreeNode* node = q.front().first;
                 q.pop();
                 
-                if (i == 0) 
-                    leftMost = cur_id;
+                if(i==0) first = cur_id;
                 
-                if (i == size - 1) 
-                    rightMost = cur_id;
- 
-                if (temp -> left)
-                    q.push({temp -> left, cur_id * 2 + 1});
+                if(i == size-1) last = cur_id;
                 
-                if (temp -> right)
-                    q.push({temp -> right, cur_id * 2 + 2});   
+                if(node->left)
+                    q.push({node->left, cur_id*2+1});
+                if(node->right)
+                    q.push({node->right, cur_id*2+2});
             }
-            ans = max(ans, rightMost - leftMost + 1);
+            ans = max(ans, last-first+1);
         }
         return ans;
     }
