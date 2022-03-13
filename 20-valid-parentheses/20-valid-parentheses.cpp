3e980206bdbf7e74fee58850a76d49266fed3fc0
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-03-13 06:17:23 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (80.05%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool isValid(string s) 
+    {
+        stack<char> st;
+        
+        for(auto it : s)
+        {
+            if(it == '(' || it == '[' || it == '{')
+            {
+                st.push(it);
+            }
+            else
+            {
+                if(st.size() == 0)
+                    return false;
+                
+                char ch = st.top();
+                
+                if((it == ')' and ch == '(') or  (it == ']' and ch == '[') or (it == '}' and ch == '{')) 
+                    st.pop();
+                else
+                    return false;
+            }
+        }
+        
+        return st.empty();
+    }
+};
