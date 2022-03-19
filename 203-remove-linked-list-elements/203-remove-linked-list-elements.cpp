7c2f910b8eb2fca5d5694eea13788f2c9714e1a0
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-03-19 21:40:55 +0530

    Time: 27 ms (68.61%), Space: 15.1 MB (48.05%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.cpp
@@ -0,0 +1,40 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int v) 
+    {        
+        if(head == NULL)
+            return head;
+        
+        while(head != NULL && head->val == v)
+            head = head->next;
+        
+        ListNode *curr = head;
+        ListNode *prev = NULL;
+               
+        while(curr != NULL)
+        {
+            if(curr->val == v)
+            {
+                prev->next = curr->next;
+                curr = curr->next;
+            }
+            else
+            {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+        
+        return head;
+    }
+};
