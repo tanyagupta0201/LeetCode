858976ec4207f798393fcc6f0b5087ea66ee699f
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-06-13 06:08:37 +0530

    Time: 85 ms (63.18%), Space: 14.9 MB (60.15%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -14,13 +14,14 @@ class Solution:
 '''
 C++ code
 
-for(int i=triangle.size()-2;i>=0;--i)
+for(int i = triangle.size() - 2; i >= 0; --i)
     {
-        for(int j=0;j<triangle[i].size();j++)
+        for(int j = 0; j < triangle[i].size(); j++)
         {
-            triangle[i][j] += min(triangle[i+1][j], triangle[i+1][j+1]);   
+            triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1]);   
         }
     }
+    
     return triangle[0][0];
 
 '''
