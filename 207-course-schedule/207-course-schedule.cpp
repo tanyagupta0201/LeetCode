3e0088a2ba78cb29a1f51e9fae93dc5e71c3cdca
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-07-03 22:20:13 +0530

    Time: 42 ms (29.98%), Space: 13.2 MB (92.23%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) 
+    {
+        vector<int> adj[numCourses];
+        vector<int> ans;
+        vector<int> indegree(numCourses, 0);
+        queue<int> q;
+        
+        for(int i = 0; i < prerequisites.size(); i++)
+        {
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+            indegree[prerequisites[i][0]]++; // adj node wale ki indegree increase hogi na 
+        }
+        
+        for(int i = 0; i < numCourses; i++)
+        {
+            if(indegree[i] == 0)
+                q.push(i);
+        }
+        
+        // Perform BFS
+        while(!q.empty())
+        {
+            int node = q.front();
+            q.pop();
+            ans.push_back(node);
+            
+            for(auto it : adj[node])
+            {
+                indegree[it]--;
+                
+                if(indegree[it] == 0)
+                {
+                    q.push(it);
+                }
+            }  
+        }
+        
+        if(ans.size() == numCourses)
+        {
+            // because it is DAG and topological sort is possible
+            return true;
+        }
+        
+        return false;
+    }
+};
