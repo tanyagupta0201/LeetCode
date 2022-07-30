eba2d3f18076c74c80324089299fb6e52433bcf1
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-07-31 01:36:34 +0530

    Time: 3 ms (38.55%), Space: 5.8 MB (97.68%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,21 @@
+// Just find the first 6 in the number and change it to 9
+// iterate over it by changing into string
+
+class Solution {
+public:
+    int maximum69Number (int num) 
+    {
+        string str = to_string(num);
+        
+        for(int i = 0; i < str.length(); i++)
+        {
+            if(str[i] == '6')
+            {
+                str[i] = '9';
+                break;
+            }
+        }
+        
+        return stoi(str);
+    }
+};
