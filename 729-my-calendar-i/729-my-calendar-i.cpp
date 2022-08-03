62a50c2fc4f97e272eb691cc4ddd19f8f70071c0
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-08-03 23:59:53 +0530

    Time: 292 ms (17.86%), Space: 38.5 MB (73.48%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -0,0 +1,19 @@
+class MyCalendar {
+    unordered_map<int, int>bookings;
+public:    
+    bool book(int s1, int e1) 
+    {
+        for(auto& [s2, e2] : bookings) 
+            if( !(s1 >= e2 || s2 >= e1) )   // if neither of above condition is satisfied, there exists an intersection
+			    return false; 
+        bookings[s1] = e1;                 // no intersection found, so insert the new slot and return true
+        return true;
+    }
+};
+
+
+/**
+ * Your MyCalendar object will be instantiated and called as such:
+ * MyCalendar* obj = new MyCalendar();
+ * bool param_1 = obj->book(start,end);
+ */
