ec9a2d8ad62cba5802e6802d48adb52edfc3ee5d
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-08-25 21:21:19 +0530

    Time: 35 ms (34.41%), Space: 8.9 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,18 +1,20 @@
 class Solution {
 public:
-    bool canConstruct(string ransomNote, string magazine) 
+    bool canConstruct(string ransomNote, string magazine)
     {
-        unordered_map<char, int> temp;
+        unordered_map<char, int> mp;
         
-        for(auto it : magazine)
-            temp[it]++;
+        for(auto it: magazine)
+            mp[it]++;
         
-        for(int i = 0; i < ransomNote.length(); i++)
+        for(int i = 0; i < ransomNote.size(); i++)
         {
-            if(--temp[ransomNote[i]] < 0)
+            // Each character in magazine should be equal or greater than the number of characters in ransomNote 
+            // If not then return false
+            if(--mp[ransomNote[i]] < 0)            
                 return false;
         }
         
-        return true;
+        return true;        
     }
 };
