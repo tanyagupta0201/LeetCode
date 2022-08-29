ade369699fc96592993621edd858b7692f9ea8ca
Tanya Gupta <66638392+tanyagupta0201@users.noreply.github.com>
2022-08-29 08:25:21 +0530

    Time: 41 ms (83.82%), Space: 12.3 MB (55.87%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -4,16 +4,17 @@ public:
 
     void markCurrLand(vector<vector<char>> &grid, int i, int j, int rows, int cols)
     {
+        // Boundary Conditions
         if(i < 0 || i >= rows || j < 0 || j >= cols || grid[i][j] != '1')
             return;
         
         // mark the curr land as visited
         grid[i][j] = '2';
         
-        markCurrLand(grid, i + 1, j, rows, cols);
-        markCurrLand(grid, i, j + 1, rows, cols);
-        markCurrLand(grid, i - 1, j, rows, cols);
-        markCurrLand(grid, i, j - 1, rows, cols);
+        markCurrLand(grid, i + 1, j, rows, cols); // DOWN
+        markCurrLand(grid, i, j + 1, rows, cols); // RIGHT
+        markCurrLand(grid, i - 1, j, rows, cols); // TOP
+        markCurrLand(grid, i, j - 1, rows, cols); // LEFT
     }
     
     int numIslands(vector<vector<char>>& grid) 
@@ -21,11 +22,14 @@ public:
         int rows = grid.size();
         int cols = grid[0].size();
         
+        // Boundary Case
+        // If the grid is empty
         if(rows == 0)
             return 0;
         
         int islands = 0;
         
+        // Iterate over all cells of the array
         for(int i = 0; i < rows; i++)
         {
             for(int j = 0; j < cols; j++)
