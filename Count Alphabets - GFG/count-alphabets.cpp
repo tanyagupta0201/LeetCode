a8668c07a20865f7898e0b2337546e71f9a3ad0a
Tanya Gupta <tanyagupta.pg@gmail.com>
2022-11-21 12:30:02 +0530

    Added count alphabets

--- Count Alphabets - GFG/count-alphabets.cpp
+++ Count Alphabets - GFG/count-alphabets.cpp
@@ -1,36 +1,16 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-int Count(string S);
-
-
-int main(){
-    int t;
-    cin >> t;
-    while(t--){
-        string S;
-        cin >> S;
-        cout << Count(S) << endl;
-    }
-return 0;
-}
-
-
-// } Driver Code Ends
-
 
 int Count(string s)
 {
-    //complete the function here
+    // complete the function here
     int cnt = 0;
-    
-    for(int i = 0; i < s.length(); i++)
+
+    for (int i = 0; i < s.length(); i++)
     {
-        if( (s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') )
+        if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z'))
         {
             cnt++;
         }
     }
-    
+
     return cnt;
 }
